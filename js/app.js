20016e291d9b72313268baea796cde3899c46324
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2021-03-16 01:13:50 +0530

    project updated

--- js/app.js
+++ js/app.js
@@ -1,10 +1,11 @@
 // this one is jut to wait for the page to load
 document.addEventListener('DOMContentLoaded', () => {
-
   const themeStylesheet = document.getElementById('theme');
   const storedTheme = localStorage.getItem('theme');
   if (storedTheme) {
     themeStylesheet.href = storedTheme;
+  } else {
+    themeStylesheet.href = './css/light-theme.css';
   }
   const themeToggle = document.getElementById('theme-toggle');
   themeToggle.addEventListener('click', () => {
@@ -18,89 +19,78 @@ document.addEventListener('DOMContentLoaded', () => {
       // themeToggle.innerText = 'Dark Mode';
     }
     // save the preference to localStorage
-    localStorage.setItem('theme', themeStylesheet.href)
-  })
-})
-
-
+    localStorage.setItem('theme', themeStylesheet.href);
+  });
+});
 
 $(document).ready(function () {
-
-
-	
   $('.nav-btn').click(function () {
-
     // displaying links on click
     $('.nav-links').slideToggle(1000);
 
     // changing nav button
     $('.nav-btn').toggleClass('turn');
-
-  })
+  });
 
   $('.nav-link').click(function () {
-
-    var mq = window.matchMedia("(max-width: 992px)");
+    var mq = window.matchMedia('(max-width: 992px)');
     if (mq.matches) {
-
       // hiding links on click
       $('.nav-links').slideToggle(1000);
 
       // changing nav button
       $('.nav-btn').toggleClass('turn');
     }
-
-  })
-
-
+  });
 
   // transparent background
   $(window).scroll(function () {
-
     let position = $(window).scrollTop();
     if (position >= 100) {
-      $('nav, nav-header').addClass('navBackground')
+      $('nav, nav-header').addClass('navBackground');
+    } else {
+      $('nav, nav-header').removeClass('navBackground');
     }
-    else {
-      $('nav, nav-header').removeClass('navBackground')
-    }
-
-	})
-
-
+  });
 
   // smooth scroll
   $('.nav-link,.light-btn').click(function (link) {
     link.preventDefault();
 
     let target = $(this).attr('href');
-    var mq = window.matchMedia("(max-width: 992px)");
+    var mq = window.matchMedia('(max-width: 992px)');
     if (mq.matches) {
-    $('html, body').stop().animate({
-      scrollTop: $(target).offset().top - 72.81
-    }, 1000);
+      $('html, body')
+        .stop()
+        .animate(
+          {
+            scrollTop: $(target).offset().top - 72.81,
+          },
+          1000
+        );
+    } else {
+      $('html, body')
+        .stop()
+        .animate(
+          {
+            scrollTop: $(target).offset().top - 68.81,
+          },
+          1000
+        );
     }
-    else {
-      $('html, body').stop().animate({
-        scrollTop: $(target).offset().top - 68.81
-      }, 1000);
-    }
-  })
-
-
+  });
 
   // isotopes
   var $grid = $('.project-images-container').isotope({
     // options
-    layoutMode: 'fitRows'
+    layoutMode: 'fitRows',
   });
   // filter items on button click
   $('.filter-button-group').on('click', 'button', function () {
     var filterValue = $(this).attr('data-filter');
     $grid.isotope({
-      filter: filterValue
+      filter: filterValue,
     });
-
   });
 
   // layout Isotope after each image loads
@@ -108,59 +98,15 @@ $(document).ready(function () {
     $grid.isotope('layout');
   });
 
-
-
-  // owl carousel for sketches
-  $('.sketches-center').owlCarousel({
-    loop: true,
-		margin: 50,
-		// autoplay:true,
-    nav: true,
-    slideBy: 1,
-    // autoplayTimeout: 3000,
-    // autoplaySpeed: 3000,
-    smartSpeed: 3000,
-    autoHeight: true,
-    responsive: {
-      0: {
-        items: 1
-      },
-      768: {
-        items: 2
-      }
-    },
-    navText: ["Prev", "Next"]
-  })
-
-
-
-	// accordion for about page
-  $(".about-page-center").accordion({
-    active:0,
-    heightStyle: "content"
+  // accordion for about page
+  $('.about-page-center').accordion({
+    active: 0,
+    heightStyle: 'content',
   });
-
-
-
-  // magnific pop up
-  $('.sketches-page-center').magnificPopup({
-    delegate: 'a', // child items selector, by clicking on it popup will open
-    type: 'image',
-    // other options
-    gallery: {
-      // options for gallery
-      enabled: true,
-      enableEscapeKey: true
-    }
-  });
-
-})
-
-
-
+});
 
 // ********** set date ************
 // select span
 const date = (document.getElementById(
-  "date"
+  'date'
 ).innerHTML = new Date().getFullYear());
